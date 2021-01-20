18d98dcfba9821bb934b587ec12791fa721efd5d
Yotam Martin <yotamm@breezometer.com>
2021-01-20 12:27:59 +0200

    utils.py with resize, download, load

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import platform
 from PIL import Image
 from torchvision import transforms
 import torchvision.transforms.functional as TF
+import matplotlib.pyplot as plt
 
 
 def download_data():
@@ -38,7 +39,25 @@ def resize(size, path):
         torch.save(image, save_path)  # if we wish float16 >>> torch.save(image.to(dtype=torch.float16), save_path))
 
 
+def load_images(path):
+    """
+        load all images from path to dictionary
+    :param path: path to .pt images
+    :return: all images as dictionary {'000001' : torch.tensor ...}
+    """
+    files_names = os.listdir(path)
+    all_images = dict()
+    for file_name in files_names:
+        image = torch.load(os.path.join(path, file_name))
+        all_images[file_name.split('.')[0]] = image
+        # plt.imshow(image.permute(1, 2, 0))
+        # plt.show()
+    return all_images
+
+
 if __name__ == '__main__':
+    # original images size == (178, 218)
     running_on_linux = 'Linux' in platform.platform()
     path = 'img_sample' if not running_on_linux else 'img_align_celeba'
     resize(size=64, path=path)
+    # load_images(path + '_pt')
